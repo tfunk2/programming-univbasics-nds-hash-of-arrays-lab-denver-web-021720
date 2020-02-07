a1861107393a422f3d5d2123baddbe56c44eae4a
Tyler Funk <tyler.j.funk2@gmail.com>
2020-02-07 20:58:28 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -9,6 +9,5 @@ def add_character(show, name)
   # Write your implementation here
   # Should return the array of the 'show' argument
   
-  show << "Dave"
-  show
+  (show[:chipmunks]).push("Snarf")
 end
